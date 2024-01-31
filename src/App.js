b66cfdb5d9fe648a7352fc46735b6716156bc9f3
kurak57 <tomitimotius92@gmail.com>
2024-01-31 08:41:31 +0700

    new route

--- src/App.js
+++ src/App.js
@@ -18,17 +18,17 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/jimat_ui_front" element={<Login/>}/>
-        <Route path="jimat_ui_front/signup" element={<SignUp/>}/>
-        <Route path="jimat_ui_front/dashboard" element={<Dashboard/>}/>
-        <Route path="jimat_ui_front/users" element={<Users/>}/>
-        <Route path="jimat_ui_front/users/add" element={<AddUser/>}/>
-        <Route path="jimat_ui_front/users/edit/:id" element={<EditUser/>}/>
-        <Route path="jimat_ui_front/interests" element={<Interests/>}/>
-        <Route path="jimat_ui_front/interests/add" element={<AddInterest/>}/>
-        <Route path="jimat_ui_front/interests/edit/:id" element={<EditInterest/>}/>
-        <Route path="jimat_ui_front/interests/mbkm/:id" element={<ProfilMbkm/>}/>
-        <Route path="jimat_ui_front/interests/minat/:id" element={<ProfilMinat/>}/>
-        <Route path="jimat_ui_front/formq" element={<AddQ/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/users/add" element={<AddUser/>}/>
+        <Route path="/users/edit/:id" element={<EditUser/>}/>
+        <Route path="/interests" element={<Interests/>}/>
+        <Route path="/interests/add" element={<AddInterest/>}/>
+        <Route path="/interests/edit/:id" element={<EditInterest/>}/>
+        <Route path="/interests/mbkm/:id" element={<ProfilMbkm/>}/>
+        <Route path="/interests/minat/:id" element={<ProfilMinat/>}/>
+        <Route path="/formq" element={<AddQ/>}/>
 
       </Routes>
       </BrowserRouter>
