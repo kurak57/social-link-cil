87f25070933d57bb97fe591512ff8c2c3100f963
kurak57 <tomitimotius92@gmail.com>
2024-02-07 15:23:38 +0700

    change some api

--- src/components/FormAddQ.jsx
+++ src/components/FormAddQ.jsx
@@ -170,7 +170,7 @@ const FormQ = () => {
 
   const saveQ = async () => {
         try {
-            await axios.post('http://localhost:5000/scores', {
+            await axios.post('https://vercel.com/kurak57s-projects/jimat-ui-back/scores', {
                 name: fd.name,
                 fakultas: fd.fakultas,
                 sf_like: sf1, sf_comp: sf2, ma_like: ma1, ma_comp: ma2, bd_like: bd1, bd_comp: bd2,
