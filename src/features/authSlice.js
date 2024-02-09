4f0ae186c1a644235796ef56938811f5d092ffff
kurak57 <tomitimotius92@gmail.com>
2024-02-09 09:23:41 +0700

    fix login api

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 
 export const LoginUser = createAsyncThunk("user/LoginUser", async(user, thunkAPI) => {
     try {
-        const response = await axios.post('http://localhost:5000/login', {
+        const response = await axios.post('https://jimat-ui-back.vercel.app/login', {
             email: user.email,
             password: user.password
         });
@@ -26,7 +26,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async(user, thunkAPI
 
 export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) => {
     try {
-        const response = await axios.get('http://localhost:5000/me');
+        const response = await axios.get('https://jimat-ui-back.vercel.app/me');
         return response.data;
     } catch (error) {
         if(error.response){
@@ -37,7 +37,7 @@ export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) => {
 });
 
 export const LogOut = createAsyncThunk("user/logout", async() => {
-        await axios.delete('http://localhost:5000/logout');
+        await axios.delete('https://jimat-ui-back.vercel.app/logout');
 });
 
 export const authSlice = createSlice({
